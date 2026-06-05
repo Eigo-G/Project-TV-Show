f1aad0012cc7f2de7f2e53f52b461e6ac4065d1e
Eigo-G <michael.english.be@gmail.comgit config --global user.name Eigo-Ggit config --global uswer.email michael.english.be@gmail.com>
2026-06-05 13:43:41 +0200

    edited code to meet lv 100 requirements

--- script.js
+++ script.js
@@ -4,16 +4,24 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+function getEpisodeCode(episode) {
+  const seasonNumber = String(episode.season).padStart(2, "0");
+  const episodeNumber = String(episode.number).padStart(2, "0");
+
+  return `S${seasonNumber}E${episodeNumber}`;
+}
+
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
 
-rootElem.innerHTML = "";
+  rootElem.innerHTML = `<p>Got ${episodeList.length} episode(s)</p>`;
 
   episodeList.forEach((episode) => {
     const episodeElem = document.createElement("section");
+    const episodeCode = getEpisodeCode(episode);
 
     episodeElem.innerHTML = `
-      <h2>${episode.name}</h2>
+      <h2>${episode.name} - ${episodeCode}</h2>
       <p>Season ${episode.season}, Episode ${episode.number}</p>
       <img src="${episode.image.medium}" alt="${episode.name}" />
       <div>${episode.summary}</div>
@@ -22,8 +30,10 @@ rootElem.innerHTML = "";
     rootElem.appendChild(episodeElem);
   });
 
+  const creditElem = document.createElement("p");
+  creditElem.innerHTML = `Data originally from <a href="https://tvmaze.com/" target="_blank">TVMaze.com</a>`;
+  rootElem.appendChild(creditElem);
 }
 
 window.onload = setup;
-
 
