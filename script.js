85e971b4a678f26edc5afc14de47dbcb011286e2
Eigo-G <michael.english.be@gmail.comgit config --global user.name Eigo-Ggit config --global uswer.email michael.english.be@gmail.com>
2026-06-05 13:22:42 +0200

    Created array that loops for each episode

--- script.js
+++ script.js
@@ -6,7 +6,24 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+rootElem.innerHTML = "";
+
+  episodeList.forEach((episode) => {
+    const episodeElem = document.createElement("section");
+
+    episodeElem.innerHTML = `
+      <h2>${episode.name}</h2>
+      <p>Season ${episode.season}, Episode ${episode.number}</p>
+      <img src="${episode.image.medium}" alt="${episode.name}" />
+      <div>${episode.summary}</div>
+    `;
+
+    rootElem.appendChild(episodeElem);
+  });
+
 }
 
 window.onload = setup;
+
+
